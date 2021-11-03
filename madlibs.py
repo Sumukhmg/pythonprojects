253a5587fe3031dcb7f2f9fbd61e2978750768ea
SUMUKH M G <83581264+sumukhmg@users.noreply.github.com>
2021-11-03 08:13:34 +0000

    my first project

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+#this is only sample of madlib.you can have different statements for it.
+adj1=input("enter first  adjective:")
+adj2=input("enter second adjective")
+adj3=input("enter third adjective")
+verb1=input("enter first verb")
+verb2=input("enter second verb")
+noun=input("enter noun")
+madlib=f"our school cafteria has really {adj1} food.just thinking about it makes my stomach {verb1}.the spaghetti is {adj2} and tastes like {noun}.one day i swear one of my meatballs started to {verb2}!.the turky tacos are totally {adj3} and look kind of like old {noun}."
+print(madlib)
