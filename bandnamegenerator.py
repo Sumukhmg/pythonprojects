05b7ae22ec2708824272900c0b5e7d1faabdb36b
SUMUKH M G <83581264+sumukhmg@users.noreply.github.com>
2021-11-06 06:16:12 +0000

    added bandnamegenerator

---
+++ bandnamegenerator.py
@@ -0,0 +1,8 @@
+#printing welcome
+print("welcome to band name generator")
+#taking cityname as input
+cityname=input("what is your city name?\n")
+#taking favfood as input
+favfood=input("what is your favourite food?\n")
+#combining cityname and favfood to show the band name
+print("The band name is " +cityname + " " + favfood)
